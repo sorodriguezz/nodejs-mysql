df22e82b678f3d9cca8f4e21fcfea0e7289dc6eb
sebastian <s.rodriguezzapata01@gmail.com>
2020-08-07 15:00:08 -0400

    cambioa español

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -5,11 +5,11 @@ const helpers = {};
 helpers.timeago = (timestamp) => {
     /*
     TODO: para ver en español modificar el archivo "index.js" en:
-    ! /node_modules/timeago.js/lib/index.js
+    * /node_modules/timeago.js/lib/index.js
     TODO: cambiar :
-    ! var en_US_1 = __importDefault(require("./lang/es"));
-    ! register_1.register('es', en_US_1.default);
-    CON ESE CAMBIO TOMARÁ LA LIBRERIA EN ESPAÑOL
+    * var en_US_1 = __importDefault(require("./lang/es"));
+    * register_1.register('es', en_US_1.default);
+    ? CON ESE CAMBIO TOMARÁ LA LIBRERIA EN ESPAÑOL
     */
 
     return format(timestamp, 'es');
